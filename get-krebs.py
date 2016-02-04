9ff7eeb1802402a6a0ac510ef04205dc72563ce1
Jack Morris <rudolphjacksonm@gmail.com>
2016-02-04 16:09:57 -0500

    Reverted back to state as of Dec.
    Need to branch off version that would detect python 3 and support it.

--- get-krebs.py
+++ get-krebs.py
@@ -1,21 +1,12 @@
-# Detect Python version and import correct modules
-import sys
-
-# Python 2.7
-if sys.version[0] < 3:
-  # Import urllib2
-  import urllib2
-  # Import RegEx
-  import re
-  #Import BeautifulSoup
-  from bs4 import beautifulsoup
-else:
-  from urllib.request import urlopen
-  import re
-  try:
-    import BeautifulSoup
-  except: ImportError
-    raise ImportError('No BeautifulSoup module found. Install BeautifulSoup before running this script.')
+#Import urllib2
+import urllib2
+#Import RegEx
+import re
+#Import BeautifulSoup
+try:
+  from bs4 import BeautifulSoup
+except: ImportError
+  raise ImportError('No BeautifulSoup module found. Install BeautifulSoup before running this script.')
 
 #Storing the URL and corresponding data in variables
 url = 'https://krebsonsecurity.com'
